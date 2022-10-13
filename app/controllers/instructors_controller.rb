f28243fe6b8dc0fe146052083586ddbfb1ea2618
Layna Oyervides <laynaoyervides@gmail.com>
2022-10-13 12:49:25 -0600

    Addinginstructors controller

---
+++ app/controllers/instructors_controller.rb
@@ -0,0 +1,26 @@
+class InstructorsController < ApplicationController
+
+    #get '/me'
+    def show
+        if current_instructor
+            render json: current_instructor, status: :ok
+        else
+            render json: { error: 'No active session'}, status: :unauthorized
+        end
+    
+    #post /signup
+    def create
+        instructor = Instructor.create(instructor_params)
+        if instructor.valid?
+            session[:instructor_id] = instructor.id
+            render json: instructor, status: :ok
+        else
+            render json: {error: instructor.errors }, status: :unprocessable_entity
+        end
+
+    private
+
+    def instructor_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
