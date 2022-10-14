cd71eca26c325e3af3743f1b0f8659335c8f0c08
Layna Oyervides <laynaoyervides@gmail.com>
2022-10-13 19:21:25 -0600

    Adding Links to NavBar

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -5,8 +5,14 @@ import {Link} from "react-router-dom"
 function NavBar() {
     return (
         <div>
-            <h1>change this content</h1>
+            <nav>
             <Link to="/login">Login</Link>
+            <Link to="/">Home</Link>
+            <Link to="/courses">Courses</Link>
+            <Link to="/learners">Learners</Link>
+            <Link to="/enrollments">Enrollments</Link>
+            <Link to="/activity">Activity</Link>
+            </nav>
         </div>
     )
 }
