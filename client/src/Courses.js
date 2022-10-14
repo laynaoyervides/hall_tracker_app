9d08d85367048904c7031a7cece1b542949e79ee
Layna Oyervides <laynaoyervides@gmail.com>
2022-10-14 12:13:15 -0600

    adding Courses FE

--- client/src/Courses.js
+++ client/src/Courses.js
@@ -1,10 +1,57 @@
-import React from "react"
+import React, {useState} from "react"
 
 function Courses() {
-    return (
+    const [courses, setCourses] = useState([])
+
+     //get list of courses
+     useEffect ( ()  => {
+        fetch("/courses")
+        .then ((r) => r.json())
+        .then ((courses) => setCourses(courses));
+    }, []);
+
+
+    // Add a new course - CREATE - 
+    const addNewCourse= (course) => {
+        setCourses([...courses, course]);
+    }
+    //handle the edit - UPDATE -
+    function handleUpdateCourse(updatedCourse){
+        const updatedCourses = courses.map((course) => {
+            if (course.id === updatedCourse.id) {
+                return updatedCourse;
+            }
+            else{
+                return course;
+            }
+        });
+        setCourses(updatedCourses);
+    }
+    // delete a course - DELETE -
+    function deleteCourse (id) {
+        const updatedCourses = courses.filter((course) => 
+            course.id !== id);
+        setCourses(updatedCourses);
+
+}
+    return(
         <div>
-            <h1>change this content</h1>
+            {courses.map((course) => (
+                <CourseDetail 
+                key={course.id}
+                courseName = {course.course_name}
+                classPeriod={course.class_period}
+                deleteCourse={deleteCourse}
+                onUpdateCourse={handleUpdateCourse}
+                courses={courses}
+                course={course}
+                instructors={instructors}
+                />
+            ))
+        }
+            <NewCourse addNewCourse={addNewCourse} instructors={instructors}/> 
         </div>
     )
 }
+
 export default Courses
