5c4581e0d78e975be6b36bd819e287356776a4b6
Layna Oyervides <laynaoyervides@gmail.com>
2022-10-23 18:32:42 -0600

    Adding SignUp component

---
+++ client/src/SignUp.js
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+function SignUp () {
+    return (
+        <div>
+            <Typography>
+                Sign up here:
+            </Typography>
+        </div>
+    )
+}
+
+export default SignUp
